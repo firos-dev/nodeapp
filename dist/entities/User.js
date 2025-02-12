9808bb2f9e645a1ec69d2417a52194c59b95b26f
firos-dev <67428789+firos-dev@users.noreply.github.com>
2025-02-12 11:01:11 +0530

    Bug fix

--- dist/entities/User.js
+++ dist/entities/User.js
@@ -8,15 +8,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -25,15 +16,37 @@ exports.User = void 0;
 const typeorm_1 = require("typeorm");
 const bcrypt_1 = __importDefault(require("bcrypt"));
 let User = class User {
-    hashPassword() {
-        return __awaiter(this, void 0, void 0, function* () {
-            this.password = this.password ? yield bcrypt_1.default.hash(this.password, 10) : null;
-        });
+    id;
+    firstName;
+    lastName;
+    dob;
+    idCard;
+    idCardS3Key;
+    phone;
+    phoneSecondary;
+    email;
+    userName;
+    password;
+    addressLineOne;
+    addressLineTwo;
+    street;
+    location;
+    city;
+    country;
+    postCode;
+    photo;
+    photoS3Key;
+    token;
+    status;
+    created;
+    updated;
+    createdAt;
+    updatedAt;
+    async hashPassword() {
+        this.password = this.password ? await bcrypt_1.default.hash(this.password, 10) : null;
     }
-    hashPasswordOnUpdate() {
-        return __awaiter(this, void 0, void 0, function* () {
-            this.password = this.password ? yield bcrypt_1.default.hash(this.password, 10) : null;
-        });
+    async hashPasswordOnUpdate() {
+        this.password = this.password ? await bcrypt_1.default.hash(this.password, 10) : null;
     }
 };
 exports.User = User;
--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -16,12 +7,12 @@ require("dotenv").config();
 const express_1 = __importDefault(require("express"));
 const db_1 = __importDefault(require("./db"));
 const app = (0, express_1.default)();
-(() => __awaiter(void 0, void 0, void 0, function* () {
+(async () => {
     try {
         if (!process.env.PORT) {
             throw new Error("Environment variables are missing");
         }
-        yield db_1.default.initialize()
+        await db_1.default.initialize()
             .then((con) => {
             con.runMigrations();
             console.log("Data Source has been initialized!");
@@ -46,5 +37,5 @@ const app = (0, express_1.default)();
         console.error(error);
         throw new Error("Unable to connect to db");
     }
-}))();
+})();
 //# sourceMappingURL=index.js.map
